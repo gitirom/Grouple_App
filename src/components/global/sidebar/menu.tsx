3b520a027f5ee03aa7859d5b73cec9955bc2444b
gitirom <wadiaromdane10@gmail.com>
2025-01-20 14:33:46 +0100

    create some conditional rendering for the sidebar menu

--- src/components/global/sidebar/menu.tsx
+++ src/components/global/sidebar/menu.tsx
@@ -1,17 +1,20 @@
 "use client"
+import { SIDEBAR_SETTINGS_MENU } from "@/constants/menus"
+import { useChannelInfo } from "@/hooks/channels"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
 import { usePathname } from "next/navigation"
 import { IChannels } from "."
-import { useChannelInfo } from "@/hooks/channels"
 
 type Props = {
     channels: IChannels[]
     optimisticChannel:
         | {
-              id: string
-              name: string
-              icon: string
-              createdAt: Date
-              groupId: string | null
+                id: string
+                name: string
+                icon: string
+                createdAt: Date
+                groupId: string | null
           }
         | undefined
     loading: boolean
@@ -47,7 +50,45 @@ const SideBarMenu = ({
     } = useChannelInfo()
 
 
-    return <div>SideBarMenu</div>
+    if (pathname.includes("settings")) {  //change sidebar UI based on settings 
+        return (
+            <div className=" flex flex-col ">
+                {SIDEBAR_SETTINGS_MENU.map((item) => 
+                    item.integration ? (
+                        userId === groupUserId && (
+                            <Link 
+                                className={cn(
+                                    "flex gap-x-2 items-center font-semibold rounded-xl text-themeTextGray hover:bg-themeGray p-2",
+                                    currentPage === "settings"
+                                    ? !item.path && "text-white"
+                                    : currentPage === item.path && "text-white"
+                                )}
+                                href={`/group/${groupid}/settings/${item.path}`}
+                                key={item.id}
+                            >
+                                {item.icon}
+                                {item.label}
+                            </Link>
+                        )
+                    ) : (
+                        <Link
+                            className={cn(
+                                "flex gap-x-2 items-center font-semibold rounded-xl text-themeTextGray hover:bg-themeGray p-2",
+                                currentPage === "settings"
+                                ? !item.path && "text-white"
+                                : currentPage === item.path && "text-white",
+                            )}
+                            href={`/group/${groupid}/settings/${item.path}`}
+                            key={item.id}
+                        >
+                            {item.icon}
+                            {item.label}
+                        </Link>
+                    )
+                )}
+            </div>
+        )
+    }
 }
 
 export default SideBarMenu
