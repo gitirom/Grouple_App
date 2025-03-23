817e339c2418267b7da0d1f81ff9c00bb98f761b
gitirom <wadiaromdane10@gmail.com>
2025-03-23 19:37:54 +0100

    feat: Add GropSideWidget component and integrate it with the group page
    - Created the group side widget componenet

--- src/app/(discover)/about/[groupid]/page.tsx
+++ src/app/(discover)/about/[groupid]/page.tsx
@@ -1,6 +1,7 @@
 import { onAuthenticatedUser } from "@/actions/auth"
 import { onGetGroupInfo } from "@/actions/groups"
 import { onGetActiveSubscription } from "@/actions/payments"
+import GroupSideWidget from "@/components/global/group-side-widget"
 import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query"
 import AboutGroup from "../_componnents/about"
 
@@ -25,15 +26,18 @@ const page = async ({params}: Props) => {
     })
 
     const userid = await onAuthenticatedUser()
-
+    
     return (
-        <HydrationBoundary state={dehydrate(query)} >
-            <div className="pt-36 pb-10 container grid grid-cols-1 lg:grid-cols-3 gap-x-10 ">
-                <div className="col-span-1 lg:col-span-2 ">
-                    <AboutGroup userid={userid.id!} groupid={params.groupid} />
+        <HydrationBoundary state={dehydrate(query)}>
+            <div className="pt-36 pb-10 container grid grid-cols-1 lg:grid-cols-3 gap-x-10">
+                <div className="col-span-1 lg:col-span-2">
+                <AboutGroup userid={userid.id!} groupid={params.groupid} />
+                </div>
+                <div className="col-span-1 relative">
+                <GroupSideWidget userid={userid.id} groupid={params.groupid} />
                 </div>
             </div>
-        </HydrationBoundary>
+    </HydrationBoundary>
     )
 }
 
